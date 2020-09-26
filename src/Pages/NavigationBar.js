ec7dab470d0c5174b3120c96b4aac895fb74a83a
Karishma Saini <ksaini7595@gmail.com>
2020-09-26 15:33:40 -0400

    updated manifest, to remove the sizes to display the icon and updated the navigation to remove the Frame interrupted erro

--- src/Pages/NavigationBar.js
+++ src/Pages/NavigationBar.js
@@ -6,7 +6,7 @@ import '../styles/Navbar.css';
 import ErikaMoyaResume from '../Documents/Erika Moya Resume.docx';
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import erika_icon from '../Images/glasses_moya.png';
+import icon from '../Images/glasses_moya.png';
 import Image from 'react-bootstrap/Image';
 
 
@@ -26,7 +26,7 @@ const notify = () => {
   <Navbar expand="lg"  className="sticky-top">
         <link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet"></link>
             <Navbar.Brand href="/home"></Navbar.Brand>
-            <Image className="erika-icon" src={erika_icon}/>
+            <Image className="erika-icon" src={icon}/>
           
            <Navbar.Toggle aria-controls="basic-navbar-nav"/>
       <Navbar.Collapse id="basic-navbar-nav">
@@ -34,7 +34,7 @@ const notify = () => {
 
             <Nav.Item className="px-2"><Nav.Link href="#about">About</Nav.Link></Nav.Item>
             <Nav.Item className="px-2"><Nav.Link href="#experience">Experience</Nav.Link></Nav.Item>
-            <Nav.Item className="px-2"><Nav.Link  href={ErikaMoyaResume}  onClick={notify}>Resume</Nav.Link></Nav.Item>
+            <Nav.Item className="px-2"><Nav.Link  href={ErikaMoyaResume} rel="noopener noreferrer" download  onClick={notify}>Resume</Nav.Link></Nav.Item>
             <Nav.Item className="px-2">
             <Nav.Link href="https://www.linkedin.com/in/erika-moya-82a608165/" to="" target="_blank">LinkedIn</Nav.Link>
             </Nav.Item>
